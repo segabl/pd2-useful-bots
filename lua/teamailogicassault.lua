57bd59bea64c3b5414d388eb1b4456a349577cd3
Sebastian Gabl <sebastian.gabl@gmx.at>
2021-03-06 00:13:01 +0100

    Add a sanity check to mark_enemy

--- lua/teamailogicassault.lua
+++ lua/teamailogicassault.lua
@@ -1,7 +1,9 @@
 TeamAILogicAssault._mark_special_chk_t = math.huge  -- hacky way to stop the vanilla special mark code
 
 function TeamAILogicAssault.mark_enemy(data, criminal, to_mark)
-	criminal:sound():say(to_mark:base():char_tweak().priority_shout .. "x_any", true)
+	if to_mark:base().char_tweak then
+		criminal:sound():say(to_mark:base():char_tweak().priority_shout .. "x_any", true)
+	end
 	managers.network:session():send_to_peers_synched("play_distance_interact_redirect", data.unit, "cmd_point")
 	data.unit:movement():play_redirect("cmd_point")
 	to_mark:contour():add("mark_enemy", true)
