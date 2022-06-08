b02efcdf78be83c8b88f89f13ce96ede8e49ccf5
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-06-09 00:29:24 +0200

    Make projectiles go through bots and converted enemies

---
+++ lua/projectilebase.lua
@@ -0,0 +1,6 @@
+-- Allow projectiles shot by players to go through bots
+Hooks:PostHook(ProjectileBase, "set_thrower_unit", "set_thrower_unit_ub", function (self)
+	if self._thrower_unit:in_slot(managers.slot:get_mask("criminals")) then
+		self._slot_mask = self._slot_mask - managers.slot:get_mask("criminals")
+	end
+end)
