0dce7508b16a756ae8c5ac8b8d437c1f2cc3b117
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-07-17 15:53:00 +0200

    Rework revive delay code
    When a Cloaker/Taser is near the player that needs to be revived bots would not start reviving before that enemy was dead, now they will also start reviving if the custody timer is getting too low

--- lua/coplogictravel.lua
+++ lua/coplogictravel.lua
@@ -1,18 +1,33 @@
 -- Stop moving towards revive target if a dangerous special is close to the bot
 local upd_advance_original = CopLogicTravel.upd_advance
 function CopLogicTravel.upd_advance(data, ...)
-	if data.objective and data.objective.type == "revive" then
-		local focus_enemy = data.attention_obj
-		if focus_enemy and focus_enemy.verified and focus_enemy.dis < 1000 and focus_enemy.unit:base() and focus_enemy.unit:base().has_tag then
-			if focus_enemy.unit:base():has_tag("spooc") or focus_enemy.unit:base():has_tag("taser") then
-				if data.internal_data.advancing then
-					data.unit:brain():action_request({
-						body_part = 2,
-						type = "idle"
-					})
-				end
-				return
+	local objective = data.objective
+	local revive_unit = objective and objective.follow_unit
+	if not objective or objective.type ~= "revive" or not alive(revive_unit) or mvector3.distance_sq(data.m_pos, revive_unit:position()) > 250000 then
+		return upd_advance_original(data, ...)
+	end
+
+	local timer = math.huge
+	if revive_unit:base().is_local_player then
+		timer = revive_unit:character_damage()._downed_timer or timer
+	elseif revive_unit:interaction().get_waypoint_time then
+		timer = revive_unit:interaction():get_waypoint_time() or timer
+	end
+
+	if timer < 5 then
+		return upd_advance_original(data, ...)
+	end
+
+	local focus_enemy = data.attention_obj
+	if focus_enemy and focus_enemy.verified and focus_enemy.dis < 1000 and focus_enemy.unit:base() and focus_enemy.unit:base().has_tag then
+		if focus_enemy.unit:base():has_tag("spooc") or focus_enemy.unit:base():has_tag("taser") then
+			if data.internal_data.advancing then
+				data.unit:brain():action_request({
+					body_part = 2,
+					type = "idle"
+				})
 			end
+			return
 		end
 	end
 
