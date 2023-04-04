c9c6eeb6fa613761824fc932b19dd01f97088d8e
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-04-04 13:22:48 +0200

    Rework revive stop code to work with clients

--- lua/interactionext.lua
+++ lua/interactionext.lua
@@ -3,34 +3,46 @@ if not Network:is_server() then
 end
 
 -- Stop bots revive objective if someone else starts reviving
-Hooks:PostHook(ReviveInteractionExt, "_at_interact_start", "_at_interact_start_ub", function (self, player)
-	self._block_revive_SO = (self._block_revive_SO or 0) + 1
+Hooks:PreHook(ReviveInteractionExt, "_at_interact_start", "_at_interact_start_ub", function (self, player)
+	self._reviving_unit = player
+end)
+
+Hooks:PreHook(ReviveInteractionExt, "_at_interact_interupt", "_at_interact_interupt_ub", function (self, player)
+	if self._reviving_unit == player then
+		self._reviving_unit = nil
+	end
+end)
+
+Hooks:PreHook(ReviveInteractionExt, "remove_interact", "remove_interact_ub", function (self)
+	UsefulBots:stop_assist_objective(self._unit)
+	self._reviving_unit = nil
+	self._block_revive_SO = nil
+end)
+
+Hooks:PostHook(ReviveInteractionExt, "set_waypoint_paused", "set_waypoint_paused_ub", function (self, paused)
+	if paused then
+		self._block_revive_SO = self._block_revive_SO and self._block_revive_SO + 1 or 1
+	else
+		self._block_revive_SO =  self._block_revive_SO and self._block_revive_SO > 1 and self._block_revive_SO - 1 or nil
+		return
+	end
 
 	local reviving_bot = UsefulBots:get_reviving_unit(self._unit)
-	if not reviving_bot or player == reviving_bot then
+	if not reviving_bot or self._reviving_unit == reviving_bot then
 		return
 	end
 
 	reviving_bot:brain():set_objective(nil)
 
+	reviving_bot:movement():action_request({
+		body_part = 4,
+		type = "stand"
+	})
+
 	if UsefulBots.settings.defend_reviving then
+		reviving_bot:brain():set_logic("assault")
 		local objective = UsefulBots:get_assist_objective(self._unit)
 		objective.in_place = objective.nav_seg == reviving_bot:movement():nav_tracker():nav_segment()
 		reviving_bot:brain():set_objective(objective)
 	end
-end)
-
-Hooks:PostHook(ReviveInteractionExt, "_at_interact_interupt", "_at_interact_interupt_ub", function (self)
-	if self._block_revive_SO then
-		if self._block_revive_SO > 1 then
-			self._block_revive_SO = self._block_revive_SO - 1
-		else
-			self._block_revive_SO = nil
-		end
-	end
-end)
-
-Hooks:PostHook(ReviveInteractionExt, "remove_interact", "remove_interact_ub", function (self)
-	UsefulBots:stop_assist_objective(self._unit)
-	self._block_revive_SO = nil
 end)
