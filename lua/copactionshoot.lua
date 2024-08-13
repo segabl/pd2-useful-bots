0b38d3bba4144b9ab1132f8aa4e9fa256f7b4712
Sebastian Gabl <sebastian.gabl@gmx.at>
2024-08-13 17:31:44 +0200

    Fix potential crash when downed bot shoots

--- lua/copactionshoot.lua
+++ lua/copactionshoot.lua
@@ -1,8 +1,7 @@
--- CopActionHurt._upd_bleedout can potentially call this function with a wrong additional parameter before the time, strip that if that's the case
+-- CopActionHurt._upd_bleedout can potentially call this function with a wrong parameters, skip it if that's the case
 local _get_unit_shoot_pos_original = CopActionShoot._get_unit_shoot_pos
 function CopActionShoot:_get_unit_shoot_pos(t_or_garbage, ...)
-	if type(t_or_garbage) ~= "number" then
-		return _get_unit_shoot_pos_original(self, ...)
+	if type(t_or_garbage) == "number" then
+		return _get_unit_shoot_pos_original(self, t_or_garbage, ...)
 	end
-	return _get_unit_shoot_pos_original(self, t_or_garbage, ...)
 end
